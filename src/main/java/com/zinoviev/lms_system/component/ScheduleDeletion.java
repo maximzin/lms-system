3ceee24e978d1ecb0f4882a8f8b98f5a3fe56bce
maxim zinovev <maximzin@mail.ru>
2026-06-23 12:58:55 +0500

    dev(fix-pr-5): add @RequiredArgsConstructor in ScheduleDeletion.java and add parameter deletion-period-cron in application.yaml

--- src/main/java/com/zinoviev/lms_system/component/ScheduleDeletion.java
+++ src/main/java/com/zinoviev/lms_system/component/ScheduleDeletion.java
@@ -1,6 +1,7 @@
 package com.zinoviev.lms_system.component;
 
 import com.zinoviev.lms_system.service.schedule.ScheduleService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ScheduleDeletion {
 
     private final ScheduleService scheduleService;
@@ -15,11 +17,7 @@ public class ScheduleDeletion {
     @Value("${schedule.duration-days-to-delete}")
     private Integer durationDaysToDelete;
 
-    public ScheduleDeletion(ScheduleService scheduleService) {
-        this.scheduleService = scheduleService;
-    }
-
-    @Scheduled(cron = "0 0 0 * * *") // каждый день в 00:00
+    @Scheduled(cron = "${schedule.deletion-period-cron}")
     public void deleteOldSchedule() {
         scheduleService.deleteOldSchedule(durationDaysToDelete);
     }
